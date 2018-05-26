98fdb1aaa05476d10589441cb731acebec6d00b0
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2018-05-26 11:36:45 +0200

    initial

---
+++ demo-solutions/binary-addition/solution.js
@@ -0,0 +1,8 @@
+function binary_addition(a,b) {
+  var sum = a + b;
+  return sum.toString(2);
+}
+
+module.exports = binary_addition;
+
+// thank you Marta: https://github.com/1n3ffbl3/Binary_Addition/blob/master/solution.js
